071ec153b44c6c86a4a6d66c825f89363192f5f2
Tim Hankinson <hammerhankinson@gmail.com>
2026-07-24 14:58:54 -0400

    Configure Drupal config management

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -260,6 +260,8 @@ $databases = [];
  */
 # $settings['config_sync_directory'] = '/directory/outside/webroot';
 
+$settings['config_sync_directory'] = $app_root . '/../config/sync';
+
 /**
  * Settings:
  *
