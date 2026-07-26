3a025a88203cedaeb94ad5830a9bd8ecace81539
Tim Hankinson <hammerhankinson@gmail.com>
2026-07-26 17:15:56 -0400

    Configure Pantheon deployment integration

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -877,6 +877,9 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 # $settings['migrate_file_public_path'] = '';
 # $settings['migrate_file_private_path'] = '';
 
+// Load Pantheon platform settings.
+include \Pantheon\Integrations\Assets::dir() . '/settings.pantheon.php';
+
 // Automatically generated include for settings managed by ddev.
 if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev.php')) {
   include __DIR__ . '/settings.ddev.php';
